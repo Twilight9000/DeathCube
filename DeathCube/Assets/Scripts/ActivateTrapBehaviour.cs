92debb3a40e727e916ab4cdb0815704ba2cfb148
Charlie1417 <cneville@mail.bradley.edu>
2023-03-06 10:49:32 -0600

    fixed some things and made universal trap activation

--- DeathCube/Assets/Scripts/ActivateTrapBehaviour.cs
+++ DeathCube/Assets/Scripts/ActivateTrapBehaviour.cs
@@ -5,29 +5,48 @@ using UnityEngine;
 public class ActivateTrapBehaviour : MonoBehaviour
 {
 
+    public GameObject spike;
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            StartTrap(spike);
+
+        }
+    }
+
     public void StartTrap(GameObject g)
     {
-        if (g.GetComponent<PitfallBehaviour>() == true)
+
+        if (g.name.Contains("Pit"))
         {
             g.GetComponent<PitfallBehaviour>().StartCoroutine("ActivateTrap");
         }
-        if (g.GetComponent<SpikeBehaviour>() == true)
+
+        if (g.name.Contains("Spike"))
         {
             g.GetComponent<SpikeBehaviour>().StartCoroutine("ActivateTrap");
         }
-        if (g.GetComponent<AxeBehaviour>() == true)
+
+        if (g.name.Contains("Axe"))
         {
             g.GetComponent<AxeBehaviour>().StartCoroutine("hell");
         }
-        if (g.GetComponent<FlyswatterBehaviour>() == true)
+
+        if (g.name.Contains("Fly"))
         {
             g.GetComponent<FlyswatterBehaviour>().activated = true;
         }
-        if (g.GetComponent<hammerBehaviour>() == true)
+
+        if (g.name.Contains("Hammer"))
         {
             g.GetComponent<hammerBehaviour>().StartCoroutine("spinHammer");
         }
-        if (g.GetComponent<Sawblade>() == true)
+
+        if (g.name.Contains("Saw"))
         {
             g.GetComponent<Sawblade>().StartCoroutine("BackAndForth");
         }
