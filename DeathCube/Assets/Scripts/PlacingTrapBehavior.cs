1906ac4ff1c5a2d1e13f04f6e6efcad88af48c49
strda <jshull@mail.bradley.edu>
2023-03-07 14:39:01 -0600

    trap placing

--- DeathCube/Assets/Scripts/PlacingTrapBehavior.cs
+++ DeathCube/Assets/Scripts/PlacingTrapBehavior.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Code for the behavior of placing down a trap.
@@ -15,9 +16,19 @@ public class PlacingTrapBehavior : MonoBehaviour
     public Camera cam;
     private Ray ray;
 
+    public GameObject buttons;
+    private int buttonsLeft;
+
+    public static List<GameObject> trapsPlaced = new List<GameObject>();
+    private bool placingTrap;
+
     private void Start()
     {
-
+        buttonsLeft = buttons.transform.childCount;
+        if(trapsPlaced.Count > 0)
+        {
+            spawnTraps();
+        }
     }
     /// <summary>
     /// Lets the player move the trap around on the screen.
@@ -29,16 +40,45 @@ public class PlacingTrapBehavior : MonoBehaviour
         ray = cam.ScreenPointToRay(Input.mousePosition);
 
         //TODO: Move the trap around either using raycast and mouse
-
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit) && placingTrap)
         {
             trapBeingPlaced.transform.position = hit.transform.position;
+            if (Input.GetMouseButtonUp(1))
+            {
+                buttonsLeft--;
+                trapsPlaced[trapsPlaced.Count - 1].transform.position = trapBeingPlaced.transform.position;
+                placingTrap = false;
+                trapBeingPlaced = null;
+                buttons.gameObject.SetActive(true);
+            }
         }
+
+        if(buttonsLeft ==0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            trapsPlaced.Clear();
+        }
+
+
     }
 
     public void PlaceTrap(int trapLoc)
     {
+        trapsPlaced.Add(traps[trapLoc]);
         trapBeingPlaced = Instantiate(traps[trapLoc]);
+        placingTrap = true;
+    }
+
+    public void spawnTraps()
+    {
+        foreach(GameObject g in trapsPlaced)
+        {
+            Instantiate(g, g.transform.position, g.transform.rotation);
+        }
     }
 
 }
