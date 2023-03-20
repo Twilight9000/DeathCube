e6e5d8d7a74c78100d4be37350d97262fcef4b29
Charlie1417 <cneville@mail.bradley.edu>
2023-03-20 09:07:53 -0500

    picking trap ui changes

---
+++ DeathCube/Assets/Scripts/RandomizeTrapsBehaviour.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomizeTrapsBehaviour : MonoBehaviour
+{
+
+    public List<GameObject> allTrapsButtons = new List<GameObject>();
+
+    public List<Vector3> placements = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < 6; i++)
+        {
+
+            GameObject g = Instantiate(allTrapsButtons[Random.Range(0, allTrapsButtons.Count)], transform);
+
+            g.transform.localPosition = placements[i];
+
+            g.GetComponent<Button>().onClick.AddListener(() => ButtonClicked(g));
+
+            int num;
+
+            if (g.name.Contains("Saw"))
+            {
+                num = 0;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Axe"))
+            {
+                num = 1;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Spikes"))
+            {
+                num = 2;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Minion"))
+            {
+                num = 3;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Hammer"))
+            {
+                num = 4;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Pitfall"))
+            {
+                num = 5;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+            else if (g.name.Contains("Fly"))
+            {
+                num = 6;
+                g.GetComponent<Button>().onClick.AddListener(() => thing(num));
+            }
+
+        }
+
+    }
+
+    public void ButtonClicked(GameObject g)
+    {
+
+        gameObject.SetActive(false);
+
+    }
+
+    public void thing(int num)
+    {
+
+        FindObjectOfType<PlacingTrapBehavior>().gameObject.GetComponent<PlacingTrapBehavior>().PlaceTrap(num);
+        Debug.Log("tada!");
+    }
+}
