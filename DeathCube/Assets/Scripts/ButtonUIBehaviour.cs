b2b121425376a48b22b121c130410795cef4201a
0Moochoo <wescamillamorque@mail.bradley.edu>
2023-03-20 10:52:22 -0500

    Changing UI for trapper room
    work in progress

---
+++ DeathCube/Assets/Scripts/ButtonUIBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUIBehaviour : MonoBehaviour
+{
+    [SerializeField] Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //image.fillAmount +=0.1f * Time.deltaTime;
+    }
+}
