97791adbdf5f787be0862bade5886ceaffda12fe
Selke Studios <selkestudioss@gmail.com>
2023-03-27 10:51:08 -0500

    bombs

--- DeathCube/Assets/Scripts/bombBehaviour.cs
+++ DeathCube/Assets/Scripts/bombBehaviour.cs
@@ -4,34 +4,53 @@ using UnityEngine;
 
 public class bombBehaviour : MonoBehaviour
 {
-    public bool isBombUnderMouseControl;
-    public Vector3 bombPos;
-    public float mouseWheelScrollSpeed;
+    public float moveSpeed = 1f;
+    public float zMovementMultiplier = 0.5f;
+    public float zMovementMin;
+    public float zMovementMax;
 
-    private void Awake()
+    public bool bombHasBeenPlaced;
+
+    private Vector3 lastMousePosition;
+
+    private void Start()
     {
-        //bombPos.x = transform.position.x;
-        //bombPos.y = transform.position.y;
+        lastMousePosition = Input.mousePosition;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        isBombUnderMouseControl = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(isBombUnderMouseControl)
+        if(!bombHasBeenPlaced)
         {
-            bombPos.z += Input.mouseScrollDelta.y * mouseWheelScrollSpeed;
-            transform.position = bombPos;
+            // Get the current mouse position and the scroll wheel value
+            Vector3 mousePosition = Input.mousePosition;
+
+            // Calculate the movement in each axis based on the change in mouse position and scroll wheel value
+            float xMovement = (mousePosition.x - lastMousePosition.x) * moveSpeed * Time.deltaTime;
+            float zMovement = (mousePosition.y / Screen.height - 0.5f) * zMovementMultiplier;
+
+            // Update the position of the object with the calculated movement
+            Vector3 position = transform.position;
+            position.x += xMovement;
+            position.z += zMovement;
+            if (position.z <= zMovementMin)
+            {
+                position.z = zMovementMin;
+            }
+            else if (position.z >= zMovementMax)
+            {
+                position.z = zMovementMax;
+            }
+            transform.position = position;
+
+            // Record the current mouse position for the next frame
+            lastMousePosition = mousePosition;
         }
 
         if(Input.GetMouseButtonDown(1))
         {
-            isBombUnderMouseControl = false;
+            bombHasBeenPlaced = true;
         }
+
     }
 }
--- DeathCube/Assets/Scripts/bombSettingBehaviour.cs
+++ DeathCube/Assets/Scripts/bombSettingBehaviour.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class bombSettingBehaviour : MonoBehaviour
 {
-    public Vector3 mousePos;
     public GameObject bomb;
 
-    private bombBehaviour BB;
+    public GameObject bombToControl;
+
+    public float spawnBombAtThisYPosition;
+
+    private void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            mousePos = Input.mousePosition;
-            mousePos.z = 2.0f;
-            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-            BB = Instantiate(bomb, objectPos, Quaternion.identity).GetComponent<bombBehaviour>();
-            BB.bombPos.x = objectPos.x;
-            BB.bombPos.y = objectPos.y;
+            bombToControl = Instantiate(bomb);
+
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f;
+            mousePosition.y = spawnBombAtThisYPosition;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            worldPosition.y += 3.18f;
+
+            bombToControl.transform.position = worldPosition;
         }
+
+        /*
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f; // Set the Z position of the prefab
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            spawnParent.transform.position = worldPosition;
+        */
     }
 }
